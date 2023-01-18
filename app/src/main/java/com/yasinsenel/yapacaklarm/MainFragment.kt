d9f2271694fed47113dd2232dee259f71a488096
Yasin Şenel <yasinsenel3437@gmail.com>
2023-01-19 02:20:42 +0300

    Main Fragment Added

---
+++ app/src/main/java/com/yasinsenel/yapacaklarm/MainFragment.kt
@@ -0,0 +1,30 @@
+package com.yasinsenel.yapacaklarm
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.yasinsenel.yapacaklarm.databinding.FragmentTodoBinding
+
+
+class MainFragment : Fragment() {
+    private lateinit var binding : FragmentTodoBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentTodoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
