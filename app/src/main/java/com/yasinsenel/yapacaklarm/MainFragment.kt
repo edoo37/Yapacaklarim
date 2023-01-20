53300432e0b15b490d2ee2be9953017e155c17d7
Yasin Şenel <yasinsenel3437@gmail.com>
2023-01-20 15:49:32 +0300

    no message

--- app/src/main/java/com/yasinsenel/yapacaklarm/MainFragment.kt
+++ app/src/main/java/com/yasinsenel/yapacaklarm/MainFragment.kt
@@ -5,11 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.yasinsenel.yapacaklarm.databinding.FragmentTodoBinding
+import androidx.navigation.Navigation
+import com.yasinsenel.yapacaklarm.databinding.FragmentMainBinding
 
 
 class MainFragment : Fragment() {
-    private lateinit var binding : FragmentTodoBinding
+    private lateinit var binding : FragmentMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -19,12 +20,15 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentTodoBinding.inflate(inflater, container, false)
+        binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.fab.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_todoFragment2_to_addTaskFragment)
+        }
 
     }
 }
